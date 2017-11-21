787c2276392378d893f158a450410939572e333b
Martin Ellis <martin.ellis@gov.scot>
2017-11-21 09:55:38 +0000

    Create project from hippo-project-archetype 12.0.2

---
+++ site/src/main/java/scot/gov/www/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package scot.gov.www.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="govscot:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
