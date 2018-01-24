58ea139ec54203cf2c3de335e50b0ca2694fce60
Martin Ellis <martin.ellis@gov.scot>
2018-01-24 14:26:52 +0000

    Add empty test
    The site project currently only contains bean classes with no tests.
    This commit adds an empty test to prevent the build failing because
    the project doesn't generate any test reports.

---
+++ site/src/test/java/scot/gov/www/beans/EmptyTest.java
@@ -0,0 +1,14 @@
+package scot.gov.www.beans;
+
+import org.junit.Test;
+
+public class EmptyTest {
+
+    @Test
+    public void emptyTest() {
+        // This project contains only bean classes.
+        // This 'test' ensures that the build generates at least one test result file, preventing the build from
+        // failing because of lack of tests.
+    }
+
+}
