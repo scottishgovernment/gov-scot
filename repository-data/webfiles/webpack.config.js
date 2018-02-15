bf074b406895260d20416375febbd60114590170
Gary Carmichael <gary.carmichael@gov.scot>
2018-02-15 13:57:57 +0000

    MGS-4039 Front end build for JavaScript sources
    * Added Webpack execution as part of build
    * Added npm dependencies for Webpack and loaders
    * Added Webpack config
    * JavaScript generated under src/main/resources, to match generated CSS,
      because this is where webfiles are autoreloaded from.

---
+++ repository-data/webfiles/webpack.config.js
@@ -0,0 +1,72 @@
+const webpack = require('webpack');
+const path = require('path');
+
+// Packs JS files & deps into bundles
+module.exports = {
+        devtool: 'source-map',
+
+        entry: {
+            'default': './src/scripts/govscot/format.default.js'
+        },
+
+        resolve: {
+            modules: [
+                './src/main/resources/site/assets/scripts',
+                'node_modules'
+            ],
+
+            extensions: ['', '.js'],
+
+            // equivalent to requirejs paths
+            alias: {
+                'jquery': '../vendor/jquery.min',
+                'moment': '../vendor/moment'
+            }
+        },
+
+        resolveLoader: {
+            alias: {
+                
+            }
+        },
+
+        output: {
+            path: path.resolve(__dirname, 'src/main/resources/site/assets/scripts'),
+            filename: '[name].js'
+        },
+
+        module: {
+            loaders: [
+                {
+                    test: /(govscot|shared|utils)\/.*\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    loader: 'babel',
+                    query: {
+                        presets: ['es2015']
+                    }
+                }
+            ]
+        },
+
+
+    development: {
+        plugins: [
+            new webpack.DefinePlugin({
+                DEBUG: true,
+                PRODUCTION: false
+            })
+        ]
+    },
+
+    production: {
+        plugins: [
+            new webpack.DefinePlugin({
+                DEBUG: false,
+                PRODUCTION: true
+            }),
+            new webpack.optimize.DedupePlugin(),
+            new webpack.optimize.UglifyJsPlugin()
+        ]
+    }
+
+};
