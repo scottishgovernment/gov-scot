76d177cf771a6d8d7e2b282e1defeb1fc4af70a7
Beth Fraser <beth.fraser@gov.scot>
2018-03-13 11:29:04 +0000

    MGS-4195 Remove JS that is not in use yet

--- repository-data/webfiles/webpack.config.js
+++ repository-data/webfiles/webpack.config.js
@@ -11,19 +11,7 @@ module.exports = {
         'global':                       './src/scripts/govscot/global.js',
 
         // format-specific entry points
-        'aps-publication':              './src/scripts/govscot/format.aps-publication.js',
-        'atoz':                         './src/scripts/govscot/format.atoz.js',
-        'default':                      './src/scripts/govscot/format.default.js',
-        'home':                         './src/scripts/govscot/format.home.js',
-        'issue-hub':                    './src/scripts/govscot/format.issue-hub.js',
-        'non-aps-publication':          './src/scripts/govscot/format.non-aps-publication.js',
-        'policy-landing-page':          './src/scripts/govscot/format.policy-landing-page.js',
-        'policy':                       './src/scripts/govscot/format.policy.js',
-        'policy-latest':                './src/scripts/govscot/format.policy-latest.js',
-        'press-release-landing-page':   './src/scripts/govscot/format.press-release-landing-page.js',
-        'publication-landing':          './src/scripts/govscot/format.publication-landing.js',
-        'search':                       './src/scripts/govscot/format.search.js',
-        'topic':                        './src/scripts/govscot/format.topic.js'
+        'policy':                       './src/scripts/govscot/format.policy.js'
     },
 
     resolve: {
