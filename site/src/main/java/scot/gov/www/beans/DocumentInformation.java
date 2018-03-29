7fd5a9c5e64d597716b421006d3de35fea39b1bb
David Sinclair <david.sinclair@gov.scot>
2018-03-29 13:59:55 +0100

    MGS-4256 add publication beans

---
+++ site/src/main/java/scot/gov/www/beans/DocumentInformation.java
@@ -0,0 +1,9 @@
+package scot.gov.www.beans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+
+@HippoEssentialsGenerated(internalName = "govscot:DocumentInformation")
+@Node(jcrType = "govscot:DocumentInformation")
+public class DocumentInformation extends BaseDocument {
+}
---
+++ site/src/main/java/scot/gov/www/beans/PublicationPage.java
@@ -0,0 +1,9 @@
+package scot.gov.www.beans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+
+@HippoEssentialsGenerated(internalName = "govscot:PublicationPage")
+@Node(jcrType = "govscot:PublicationPage")
+public class PublicationPage extends BaseDocument {
+}
