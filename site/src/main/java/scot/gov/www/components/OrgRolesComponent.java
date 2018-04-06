ae825a957da038bef057090e70b77cb8783addad
David Sinclair <david.sinclair@gov.scot>
2018-04-06 14:54:38 +0100

    wip

--- site/src/main/java/scot/gov/www/components/OrgRolesComponent.java
+++ site/src/main/java/scot/gov/www/components/OrgRolesComponent.java
@@ -1,4 +1,61 @@
 package scot.gov.www.components;
 
-public class OrgRolesComponent {
+import org.hippoecm.hst.component.support.bean.BaseHstComponent;
+import org.hippoecm.hst.content.beans.query.HstQuery;
+import org.hippoecm.hst.content.beans.query.HstQueryResult;
+import org.hippoecm.hst.content.beans.query.exceptions.QueryException;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+import org.hippoecm.hst.core.request.HstRequestContext;
+import org.hippoecm.hst.util.ContentBeanUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scot.gov.www.beans.Directorate;
+import scot.gov.www.beans.OrgRoles;
+import scot.gov.www.beans.Person;
+import scot.gov.www.beans.Policy;
+import scot.gov.www.beans.Role;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OrgRolesComponent  extends BaseHstComponent {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DirectorateComponent.class);
+
+    @Override
+    public void doBeforeRender(final HstRequest request,
+                               final HstResponse response) {
+
+        HstRequestContext context = request.getRequestContext();
+        OrgRoles document = context.getContentBean(OrgRoles.class);
+        request.setAttribute("document", document);
+
+        //
+    }
+
+
+    class PersonWithRoles {
+        private Person person;
+        private List<Role> roles;
+
+        public Person getPerson() {
+            return person;
+        }
+
+        public void setPerson(Person person) {
+            this.person = person;
+        }
+
+        public List<Role> getRoles() {
+            return roles;
+        }
+
+        public void setRoles(List<Role> roles) {
+            this.roles = roles;
+        }
+    }
+
 }
