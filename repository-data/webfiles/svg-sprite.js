69de8a0774c485513ce3a12c0e69897b3e9efeab
Jonathan Sutcliffe <Jonathan.Sutcliffe@gov.scot>
2018-04-17 16:51:18 +0100

    [NO JIRA] add SVG sprite task

---
+++ repository-data/webfiles/svg-sprite.js
@@ -0,0 +1,47 @@
+'use strict';
+//svg-sprite ../../site/src/main/webapp/assets/images/icons/svg/*.svg --shape-id-separator='' -S -V=false -s=false --Ss=icons.stack.svg --stack-dest='../../site/src/main/webapp/assets/images/icons'
+var SVGSpriter = require('svg-sprite'),
+path = require('path'),
+mkdirp = require('mkdirp'),
+fs = require('fs'),
+File = require('vinyl'),
+glob = require('glob'),
+
+config = {
+    "shape": {
+        "id": {
+           "separator": ""
+        }
+    },
+    "mode": {
+        "stack": {
+            "dest": "../../site/src/main/webapp/assets/images/icons",
+            "sprite": "icons.stack.svg"
+        }
+    }
+},
+spriter = new SVGSpriter(config);
+
+// Register some SVG files with the spriter
+let cwd = path.resolve('../../site/src/main/webapp/assets/images/icons/svg/');
+glob.glob('**/*.svg', { cwd: cwd }, function (err, files) {
+    files.forEach(function (file, index) {
+        spriter.add(new File({
+            path: path.join(cwd, file),
+            base: cwd,
+            contents: fs.readFileSync(path.join(cwd, file))
+        }));
+    });
+
+    // Compile the sprite
+    spriter.compile(function(error, result, cssData) {
+        // Run through all configured output modes
+        for (var mode in result) {
+            // Run through all created resources and write them to disk
+            for (var type in result[mode]) {
+                mkdirp.sync(path.dirname(result[mode][type].path));
+                fs.writeFileSync(result[mode][type].path, result[mode][type].contents);
+            }
+        }
+    });
+});
