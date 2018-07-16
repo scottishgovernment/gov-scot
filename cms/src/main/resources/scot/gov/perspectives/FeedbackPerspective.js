2900458dcc1c6333dd03f183949da56b2054ff84
Jonathan Sutcliffe <Jonathan.Sutcliffe@gov.scot>
2018-07-16 11:33:21 +0100

    MGS-4598 detect current publishing env with JS, to display the correct Rubric feedback

--- cms/src/main/resources/scot/gov/perspectives/FeedbackPerspective.js
+++ cms/src/main/resources/scot/gov/perspectives/FeedbackPerspective.js
@@ -1,16 +1,37 @@
 let FeedbackPerspective;
 
 (function () {
+    let environments = {
+        lcl: 'https://lgv.publishing.gov.scot',
+        dev: 'https://dgv.publishing.gov.scot',
+        int: 'https://igv.publishing.gov.scot',
+        exp: 'https://egv.publishing.gov.scot',
+        per: 'https://pgv.publishing.gov.scot',
+        tst: 'https://tgv.publishing.gov.scot',
+        uat: 'https://ugv.publishing.gov.scot',
+        blu: 'https://bgv.publishing.gov.scot',
+        grn: 'https://ggv.publishing.gov.scot',
+        www: 'https://beta.publishing.gov.scot'
+    };
+
     if (typeof FeedbackPerspective == 'undefined') {
         FeedbackPerspective = {};
         FeedbackPerspective.resizeEventRegisteredMap = [];
         FeedbackPerspective.iFrameRendered = false;
     }
 
+
     FeedbackPerspective.showIFrame = function (id) {
         if (!FeedbackPerspective.iFrameRendered) {
-            let iFrame = $('#feedback-perspective').find('iframe');
-            iFrame.attr('src', 'https://lgv.publishing.gov.scot/#/feedback');
+            let iframe = $('#feedback-perspective').find('iframe');
+
+            let currentEnvironmentKey = window.location.host.substring(0, window.location.host.indexOf('.'));
+
+            if (currentEnvironmentKey.length === 0) {
+                currentEnvironmentKey = 'lcl';
+            }
+
+            iframe.attr('src', environments[currentEnvironmentKey] + '/#/feedback');
 
             FeedbackPerspective.iFrameRendered = true;
         }
