50831c65ca6ed3246993db7fb4f4a32d1a6780b9
David Sinclair <david.sinclair@gov.scot>
2018-07-16 09:01:50 +0100

    Combine PolicyLatestComponent with PolicyComponent to avoid duplicated code

--- site/src/main/java/scot/gov/www/components/PolicyComponent.java
+++ site/src/main/java/scot/gov/www/components/PolicyComponent.java
@@ -1,17 +1,31 @@
 package scot.gov.www.components;
 
+import org.apache.commons.lang.time.StopWatch;
 import org.hippoecm.hst.component.support.bean.BaseHstComponent;
+import org.hippoecm.hst.content.beans.query.HstQuery;
+import org.hippoecm.hst.content.beans.query.HstQueryResult;
+import org.hippoecm.hst.content.beans.query.builder.Constraint;
+import org.hippoecm.hst.content.beans.query.builder.HstQueryBuilder;
+import org.hippoecm.hst.content.beans.query.exceptions.QueryException;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.content.beans.standard.HippoBeanIterator;
 import org.hippoecm.hst.core.component.HstComponentException;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scot.gov.www.beans.News;
 import scot.gov.www.beans.Policy;
 import scot.gov.www.beans.PolicyInDetail;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+import static org.hippoecm.hst.content.beans.query.builder.ConstraintBuilder.constraint;
+import static org.hippoecm.hst.content.beans.query.builder.ConstraintBuilder.or;
 
 /**
  * Get the information required to render a Policy or Policy in Detail page.
@@ -56,6 +70,46 @@ public class PolicyComponent extends BaseHstComponent {
         request.setAttribute("policyDetails", details);
         request.setAttribute("prev", prev);
         request.setAttribute("next", next);
+
+        // if this is the patest page then also include latest info
+        if (request.getPathInfo().endsWith("/latest/")) {
+            HippoBeanIterator newsIt = getLatestNews(request, policy);
+            List<HippoBean> all = new ArrayList<>();
+            newsIt.forEachRemaining(all::add);
+            all.addAll(policy.getRelatedItems());
+            Collections.sort(all, Comparator.comparing(bean -> bean.getProperty("govscot:publicationDate")));
+            Collections.reverse(all);
+            LOG.info("{} related items, total size is {}", policy.getRelatedItems().size(), all.size());
+            request.setAttribute("latest", all);
+        }
+    }
+
+    private HippoBeanIterator getLatestNews(HstRequest request, Policy policy) {
+
+        HippoBean scope = request.getRequestContext().getSiteContentBaseBean();
+        HstQuery query = HstQueryBuilder.create(scope)
+                .ofTypes(News.class)
+                .where(or(tagConstraints(policy)))
+                .orderByDescending("govscot:publicationDate").build();
+        try {
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.start();
+            HstQueryResult result = query.execute();
+            stopWatch.stop();
+            LOG.info("result count: {}, took: {}", result.getTotalSize(), stopWatch.getTime());
+            return result.getHippoBeans();
+        } catch (QueryException e) {
+            throw new HstComponentException(e);
+        }
+    }
+
+    private Constraint[] tagConstraints(Policy policy) {
+        ArrayList<Constraint> tagConstraints = new ArrayList<>();
+        for (String tag : policy.getPolicyTags()) {
+            LOG.info("policy tag: {}", tag);
+            tagConstraints.add(constraint("govscot:policyTags").equalTo(tag));
+        }
+        return tagConstraints.toArray(new Constraint[tagConstraints.size()]);
     }
 
     private Policy getPolicy(HippoBean document) {
