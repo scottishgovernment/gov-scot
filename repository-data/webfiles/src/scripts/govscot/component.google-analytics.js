c68fde43ac3979eeeb61aec057450e64a660fcf6
Jonathan Sutcliffe <Jonathan.Sutcliffe@gov.scot>
2018-08-29 16:41:24 +0100

    Update Google Analytics script to set userType in the correct part of the dataLayer.

--- repository-data/webfiles/src/scripts/govscot/component.google-analytics.js
+++ repository-data/webfiles/src/scripts/govscot/component.google-analytics.js
@@ -2,6 +2,6 @@ define([], function () {
     // sets the user type in the dataLayer
     $.get('/service/usertype', function (data) {
         window.dataLayer = window.dataLayer || [];
-        window.dataLayer.userType = data.userType;
+        window.dataLayer[0].userType = data.userType;
     });
 });
