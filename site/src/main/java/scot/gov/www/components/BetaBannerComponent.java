f35e1409d830085ce4aaeb1d3ba2854c8e35283e
Jonathan Sutcliffe <Jonathan.Sutcliffe@gov.scot>
2018-08-29 11:49:10 +0100

    Update name of BetaBannerComponent to be more specific

--- site/src/main/java/scot/gov/www/components/BetaBannerComponent.java
+++ site/src/main/java/scot/gov/www/components/BetaBannerComponent.java
@@ -6,7 +6,7 @@ import org.hippoecm.hst.core.component.HstResponse;
 import org.hippoecm.hst.core.request.HstRequestContext;
 
 
-public class BannerComponent extends BaseHstComponent {
+public class BetaBannerComponent extends BaseHstComponent {
 
     @Override
     public void doBeforeRender(HstRequest request, HstResponse response) {
@@ -19,8 +19,5 @@ public class BannerComponent extends BaseHstComponent {
         if (!"www".equals(hostGroupName)) {
             request.setAttribute("showBetaBanner", true);
         }
-
-        request.setAttribute("footrue", true);
-        request.setAttribute("barfalse", false);
     }
 }
