442d661696708e4ebdb1856cbc2609ed14cab80c
David Sinclair <david.sinclair@gov.scot>
2018-09-03 15:14:17 +0100

    MGS-4725 Use requestContext.getResolvedMount().getResolvedVirtualHost().getVirtualHost().getHostGroupName() over getHeader(host) to determine archive redirects

--- site/src/main/java/scot/gov/www/components/ArchiveUtils.java
+++ site/src/main/java/scot/gov/www/components/ArchiveUtils.java
@@ -3,6 +3,7 @@ package scot.gov.www.components;
 import org.apache.commons.lang.StringUtils;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
+import org.hippoecm.hst.core.request.HstRequestContext;
 import org.hippoecm.hst.util.HstResponseUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,12 +28,14 @@ public class ArchiveUtils {
     public static void sendArchiveRedirect(String path, HstRequest request, HstResponse response) {
 
         String archiveUrl;
-
-        if (request.getHeader("Host").endsWith("beta.gov.scot")) {
-            archiveUrl = String.format("https://www.gov.scot%s", path);
-        } else {
+        final HstRequestContext requestContext = request.getRequestContext();
+        String hostGroupName = requestContext.getResolvedMount().getMount().getVirtualHost().getHostGroupName();
+        if ("www".equals(hostGroupName)) {
             archiveUrl = String.format("https://www2.gov.scot%s", path);
+        } else {
+            archiveUrl = String.format("https://www.gov.scot%s", path);
         }
+
         LOG.info("Redirecting to archive {} -> {}", path, archiveUrl);
         HstResponseUtils.sendRedirect(request, response, archiveUrl);
     }
