ad35d1949637fc4d96131f44bec707840aa58803
Jonathan Sutcliffe <Jonathan.Sutcliffe@gov.scot>
2018-10-04 12:12:35 +0100

    Modification sto styling of complex document format
    1) remove default table border and make table header more distinct
    2) tidy up display of images and allow them to overflow into the right
       column to let them display at a larger size
    3) expand line height on complex document contents accordion to
       support multi-line titles
    4) make the definition of a given page's standard stand out more
    5) correct a defect with the display of nested lists (ol with ul children)
    6) make links within titles have an underline

--- repository-data/webfiles/src/scripts/govscot/format.complex-document.js
+++ repository-data/webfiles/src/scripts/govscot/format.complex-document.js
@@ -9,6 +9,7 @@ define([
     complexDocumentPage.init = function () {
         displayToggle.init();
         checkDocumentTitleSticky();
+        tweakComplexDocumentMarkup();
         
         $(window).on('scroll resize', function () {
             checkDocumentTitleSticky();
@@ -49,6 +50,35 @@ define([
                 $('.section-marker__document-title').removeClass('section-marker__document-title--visible')
             }
         }
+    }
+
+    function tweakComplexDocumentMarkup () {
+        // 1. add overflow class to image containers
+        $('.bs_figure').addClass('overflow--medium--three-twelfths');
+
+        // 2. style standard definition block
+        $('blockquote.bs_blockquote').addClass('info-note blockquote');
+
+        // 2.1 define elements
+        let limitationTitle, standardTitle, newLimitationTitle, newStandardTitle;
+
+        $('blockquote.bs_blockquote p').each(function (k, v) {
+            if ($(this).text().toLowerCase() === "limitation:") {
+                limitationTitle = $(this);
+            }
+        });
+
+        // 2.2 replace elements
+        newLimitationTitle = $("<h5 class=delta/>");
+        newLimitationTitle.html(limitationTitle.text());
+        newLimitationTitle.insertAfter(limitationTitle);
+        limitationTitle.remove();
+
+        standardTitle = $('blockquote.bs_blockquote .bs_blockquote-title');
+        newStandardTitle = $("<h4 class=\"beta no-top-margin\"/>");
+        newStandardTitle.html(standardTitle.html());
+        newStandardTitle.insertAfter(standardTitle);
+        standardTitle.remove();
     }
 
     return complexDocumentPage;
