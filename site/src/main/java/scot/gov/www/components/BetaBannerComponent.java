a41781d794b66b588116673adac91db8c059aec0
Martin Ellis <martin.ellis@gov.scot>
2018-11-07 13:09:57 +0000

    Remove method that only calls the same method in super-class

--- site/src/main/java/scot/gov/www/components/BetaBannerComponent.java
+++ site/src/main/java/scot/gov/www/components/BetaBannerComponent.java
@@ -1,14 +1,7 @@
 package scot.gov.www.components;
 
 import org.hippoecm.hst.component.support.bean.BaseHstComponent;
-import org.hippoecm.hst.core.component.HstRequest;
-import org.hippoecm.hst.core.component.HstResponse;
 
 public class BetaBannerComponent extends BaseHstComponent {
-
-    @Override
-    public void doBeforeRender(HstRequest request, HstResponse response) {
-        super.doBeforeRender(request, response);
-    }
 
 }
