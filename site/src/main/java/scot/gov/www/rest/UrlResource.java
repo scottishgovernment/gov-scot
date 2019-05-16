567421831c6899c4ce4947ae421fd75cab3a7faa
David Sinclair <david.sinclair@gov.scot>
2019-05-16 09:17:52 +0100

    MGS-5090 Sonar

--- site/src/main/java/scot/gov/www/rest/UrlResource.java
+++ site/src/main/java/scot/gov/www/rest/UrlResource.java
@@ -7,8 +7,6 @@ import org.hippoecm.hst.jaxrs.services.AbstractResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
-import scot.gov.www.rest.metadata.MetadataResource;
-import scot.gov.www.rest.metadata.MetadataResponse;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
