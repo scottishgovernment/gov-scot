8374e23f54383877db69e3553acb49dc6ff19459
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2019-07-01 12:59:58 +0100

    MGS-5309 Workaround: calls to userType endpoint intermittently failing on SCOTS - provide fallback

--- repository-data/webfiles/src/scripts/govscot/component.google-analytics.js
+++ repository-data/webfiles/src/scripts/govscot/component.google-analytics.js
@@ -6,6 +6,9 @@
 
 import $ from 'jquery';
 
+window.dataLayer = window.dataLayer || [];
+window.dataLayer[0].userType = 'unspecified';
+
 // sets the user type in the dataLayer
 $.get('/service/usertype', function (data) {
     window.dataLayer = window.dataLayer || [];
