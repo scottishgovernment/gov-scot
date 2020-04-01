bd0b0fd42a0ab5a7bd81ef50477e324456afcf9d
David Sinclair <david.sinclair@gov.scot>
2020-04-01 12:18:20 +0100

    MGS-5515 make the order of the lhs menu the same as that in the repo

--- site/src/main/java/scot/gov/www/components/RepoBasedMenuItem.java
+++ site/src/main/java/scot/gov/www/components/RepoBasedMenuItem.java
@@ -7,7 +7,6 @@ import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.sitemenu.EditableMenuItem;
 import org.hippoecm.hst.core.sitemenu.EditableMenuItemImpl;
 
-
 public class RepoBasedMenuItem extends EditableMenuItemImpl {
 
     public RepoBasedMenuItem(
@@ -41,7 +40,7 @@ public class RepoBasedMenuItem extends EditableMenuItemImpl {
 
         this.hstLink = request.getRequestContext().getHstLinkCreator().create(repoItem, request.getRequestContext());
 
-        if (currentContentBean!= null && repoItem.isSelf(currentContentBean)) {
+        if (currentContentBean != null && repoItem.isSelf(currentContentBean)) {
             this.selected = true;
             this.getEditableMenu().setSelectedMenuItem(this);
         }
@@ -50,25 +49,37 @@ public class RepoBasedMenuItem extends EditableMenuItemImpl {
             return;
         }
 
-        for (HippoDocumentBean childDocumentItem : repoItem.getDocuments()) {
-            EditableMenuItem childMenuItem = new RepoBasedMenuItem(childDocumentItem, this, request, currentContentBean);
-
-            if ("index".equals(childDocumentItem.getName())) {
-                // don't add the item as a child if it is the 'index' item for that folder
-                this.name = childDocumentItem.getDisplayName();
-                this.selected = childMenuItem.isSelected();
-            } else {
-                this.name = this.name.substring(0, 1).toUpperCase() + this.name.substring(1);
-                this.addChildMenuItem(childMenuItem);
-            }
+        for (HippoBean child : repoItem.getChildBeans(HippoBean.class)) {
+            processChild(child, request, currentContentBean);
         }
-
-        for (HippoFolderBean childRepoItem : repoItem.getFolders()) {
-            EditableMenuItem childMenuItem = new RepoBasedMenuItem(childRepoItem, this, request,
-                    currentContentBean);
-            this.addChildMenuItem(childMenuItem);
-        }
-
     }
 
+    private void processChild(HippoBean child, HstRequest request, HippoBean currentContentBean) {
+        if (child instanceof HippoDocumentBean) {
+            processChildDocument(child, request, currentContentBean);
+        }
+
+        if (child instanceof HippoFolderBean) {
+            processChildFolder(child, request, currentContentBean);
+        }
+    }
+
+    private void processChildDocument(HippoBean child, HstRequest request, HippoBean currentContentBean) {
+        HippoDocumentBean childDocumentItem = (HippoDocumentBean) child;
+        EditableMenuItem childMenuItem = new RepoBasedMenuItem(childDocumentItem, this, request, currentContentBean);
+        if ("index".equals(childDocumentItem.getName())) {
+            // don't add the item as a child if it is the 'index' item for that folder
+            this.name = childDocumentItem.getDisplayName();
+            this.selected = childMenuItem.isSelected();
+        } else {
+            this.name = this.name.substring(0, 1).toUpperCase() + this.name.substring(1);
+            this.addChildMenuItem(childMenuItem);
+        }
+    }
+
+    private void processChildFolder(HippoBean child, HstRequest request, HippoBean currentContentBean) {
+        HippoFolderBean childRepoItem = (HippoFolderBean) child;
+        EditableMenuItem childMenuItem = new RepoBasedMenuItem(childRepoItem, this, request, currentContentBean);
+        this.addChildMenuItem(childMenuItem);
+    }
 }
