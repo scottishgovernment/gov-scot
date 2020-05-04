uk-gov-mirror/scottishgovernment.gov-scot

MGS-5834 Added validator to news and refactored duplicate code used by news and publications for slugs. Fixed bug with publications missing validation

cms/src/main/java/scot/gov/www/SlugDaemonModule.java
@@ -1,0 +1,61 @@
+package scot.gov.www;
+
+import org.onehippo.repository.modules.DaemonModule;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryResult;
+
+/**
+ * Created by z441571 on 22/04/2020.
+ */
+public abstract class SlugDaemonModule implements DaemonModule {
+
+    protected static final String GOVSCOT_SLUG_PROPERTY = "govscot:slug";
+
+    protected Session session;
+
+    protected String allocate(String slug, String docType) throws RepositoryException {
+        // If it does not already exist then just use this slug.
+        if (!slugAlreadyExists(slug, docType)) {
+            return slug;
+        }
+
+        // The slug is already used, try appending a number starting from 2
+        // i.e. rather than try my-document-1 we will start from my-document-2.
+        return disambiguate(slug, docType, 2);
+    }
+
+    /**
+     * Recursively try adding a number to the end of the slug until we get a unique one.
+     */
+    private String disambiguate(String slug, String docType, int postfix) throws RepositoryException {
+        String candidate = String.format("%s-%d", slug, postfix);
+
+        if (!slugAlreadyExists(candidate, docType)) {
+            // Base case: we have found a unique slug.
+            return candidate;
+        }
+
+        // Recursive call to try the next number.
+        return disambiguate(slug, docType, postfix + 1);
+    }
+    private boolean slugAlreadyExists(String slug, String docType) throws RepositoryException {
+        String sql = String.format("SELECT * FROM %s WHERE govscot:slug = '%s'", docType, slug);
+        QueryResult result = session.getWorkspace().getQueryManager().createQuery(sql, Query.SQL).execute();
+        return result.getNodes().hasNext();
+    }
+
+    protected static Node getLatestVariant(Node handle) throws RepositoryException {
+        NodeIterator it = handle.getNodes();
+        Node variant = null;
+        while (it.hasNext()) {
+            variant = it.nextNode();
+        }
+        return variant;
+    }
+
+}

cms/src/main/java/scot/gov/www/validation/SlugValidator.java
@@ -9,6 +9,7 @@ import org.hippoecm.frontend.session.UserSession;
 import org.hippoecm.frontend.validation.IFieldValidator;
 import org.hippoecm.frontend.validation.ValidationException;
 import org.hippoecm.frontend.validation.Violation;
+import org.slf4j.LoggerFactory;
 
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
@@ -25,9 +26,11 @@ import java.util.Set;
  * https://www.onehippo.org/library/concepts/plugins/create-a-custom-field-validator.html
  *
  */
-public class PublicationSlugValidator extends AbstractCmsValidator {
+public class SlugValidator extends AbstractCmsValidator {
 
-    public PublicationSlugValidator(IPluginContext context, IPluginConfig config) {
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(SlugValidator.class);
+
+    public SlugValidator(IPluginContext context, IPluginConfig config) {
         super(context, config);
     }
 
@@ -56,7 +59,9 @@ public class PublicationSlugValidator extends AbstractCmsValidator {
         Set<Violation> violations = new HashSet<>();
         String candidateSlug = (String) childModel.getObject();
         try {
-            if (!isValid(candidateSlug, node)) {
+            String type = node.getPrimaryNodeType().getName();
+            LOG.info("Content path: {}", type);
+            if (!isValid(candidateSlug, type, node)) {
                 violations.add(fieldValidator.newValueViolation(childModel, getTranslation()));
             }
             return violations;
@@ -69,10 +74,11 @@ public class PublicationSlugValidator extends AbstractCmsValidator {
      * The slug is valid if no other nodes share this slug
      * (with the exception of ones that share the same handle).
      */
-    private boolean isValid(String candidateSlug, Node node) throws RepositoryException {
+    public boolean isValid(String documentType, String candidateSlug, Node node) throws RepositoryException {
         Session session = UserSession.get().getJcrSession();
-        String sql = String.format("SELECT * FROM govscot:SimpleContent WHERE govscot:slug = '%s'", candidateSlug);
+        String sql = String.format("SELECT * FROM %s WHERE govscot:slug = '%s'", documentType, candidateSlug);
         Query query = session.getWorkspace().getQueryManager().createQuery(sql, Query.SQL);
+        LOG.info("Query being run: {}", query);
         QueryResult result = query.execute();
 
         // if any of the results do not belong to the same handle then this slug is already used elsewhere

scottishgovernment/gov-scot

cms/src/main/java/scot/gov/www/SlugDaemonModule.java
@@ -1,0 +1,56 @@
+package scot.gov.www;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryResult;
+
+/**
+ * Created by z441571 on 22/04/2020.
+ */
+public abstract class SlugDaemonModule extends DaemonModuleBase {
+
+    protected static final String GOVSCOT_SLUG_PROPERTY = "govscot:slug";
+
+    protected String allocate(String slug, String docType) throws RepositoryException {
+        // If it does not already exist then just use this slug.
+        if (!slugAlreadyExists(slug, docType)) {
+            return slug;
+        }
+
+        // The slug is already used, try appending a number starting from 2
+        // i.e. rather than try my-document-1 we will start from my-document-2.
+        return disambiguate(slug, docType, 2);
+    }
+
+    /**
+     * Recursively try adding a number to the end of the slug until we get a unique one.
+     */
+    private String disambiguate(String slug, String docType, int postfix) throws RepositoryException {
+        String candidate = String.format("%s-%d", slug, postfix);
+
+        if (!slugAlreadyExists(candidate, docType)) {
+            // Base case: we have found a unique slug.
+            return candidate;
+        }
+
+        // Recursive call to try the next number.
+        return disambiguate(slug, docType, postfix + 1);
+    }
+    private boolean slugAlreadyExists(String slug, String docType) throws RepositoryException {
+        String sql = String.format("SELECT * FROM %s WHERE govscot:slug = '%s'", docType, slug);
+        QueryResult result = session.getWorkspace().getQueryManager().createQuery(sql, Query.SQL).execute();
+        return result.getNodes().hasNext();
+    }
+
+    protected static Node getLatestVariant(Node handle) throws RepositoryException {
+        NodeIterator it = handle.getNodes();
+        Node variant = null;
+        while (it.hasNext()) {
+            variant = it.nextNode();
+        }
+        return variant;
+    }
+
+}

cms/src/main/java/scot/gov/www/validation/SlugValidator.java
@@ -9,6 +9,7 @@ import org.hippoecm.frontend.session.UserSession;
 import org.hippoecm.frontend.validation.IFieldValidator;
 import org.hippoecm.frontend.validation.ValidationException;
 import org.hippoecm.frontend.validation.Violation;
+import org.slf4j.LoggerFactory;
 
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
@@ -25,9 +26,11 @@ import java.util.Set;
  * https://www.onehippo.org/library/concepts/plugins/create-a-custom-field-validator.html
  *
  */
-public class PublicationSlugValidator extends AbstractCmsValidator {
+public class SlugValidator extends AbstractCmsValidator {
 
-    public PublicationSlugValidator(IPluginContext context, IPluginConfig config) {
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(SlugValidator.class);
+
+    public SlugValidator(IPluginContext context, IPluginConfig config) {
         super(context, config);
     }
 
@@ -56,7 +59,9 @@ public class PublicationSlugValidator extends AbstractCmsValidator {
         Set<Violation> violations = new HashSet<>();
         String candidateSlug = (String) childModel.getObject();
         try {
-            if (!isValid(candidateSlug, node)) {
+            String type = node.getPrimaryNodeType().getName();
+            LOG.info("Content path: {}", type);
+            if (!isValid(type, candidateSlug, node)) {
                 violations.add(fieldValidator.newValueViolation(childModel, getTranslation()));
             }
             return violations;
@@ -69,10 +74,11 @@ public class PublicationSlugValidator extends AbstractCmsValidator {
      * The slug is valid if no other nodes share this slug
      * (with the exception of ones that share the same handle).
      */
-    private boolean isValid(String candidateSlug, Node node) throws RepositoryException {
+    public boolean isValid(String documentType, String candidateSlug, Node node) throws RepositoryException {
         Session session = UserSession.get().getJcrSession();
-        String sql = String.format("SELECT * FROM govscot:SimpleContent WHERE govscot:slug = '%s'", candidateSlug);
+        String sql = String.format("SELECT * FROM %s WHERE govscot:slug = '%s'", documentType, candidateSlug);
         Query query = session.getWorkspace().getQueryManager().createQuery(sql, Query.SQL);
+        LOG.info("Query being run: {}", query);
         QueryResult result = query.execute();
 
         // if any of the results do not belong to the same handle then this slug is already used elsewhere