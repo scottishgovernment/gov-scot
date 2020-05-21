dc0ac625d03252274acde88889acd32031066276
David Sinclair <david.sinclair@gov.scot>
2020-05-21 08:15:30 +0100

    Sonar

--- site/src/main/java/scot/gov/www/beans/AboutTopic.java
+++ site/src/main/java/scot/gov/www/beans/AboutTopic.java
@@ -1,11 +1,7 @@
 package scot.gov.www.beans;
 
 import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
-
-import java.util.List;
-import scot.gov.www.beans.AdditionalContent;
 
 @HippoEssentialsGenerated(internalName = "govscot:AboutTopic")
 @Node(jcrType = "govscot:AboutTopic")
