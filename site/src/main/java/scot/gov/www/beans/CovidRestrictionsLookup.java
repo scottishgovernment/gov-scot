b3539c2eb671190e6d8c07184ad3d185fcf6de70
Gary Carmichael <gary.carmichael@gov.scot>
2020-10-23 19:38:21 +0100

    MGS-6096 Fix incorrect bean/doctype name

---
+++ site/src/main/java/scot/gov/www/beans/CovidRestrictionsLookup.java
@@ -0,0 +1,9 @@
+package scot.gov.www.beans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+
+@HippoEssentialsGenerated(internalName = "govscot:CovidRestrictionsLookup")
+@Node(jcrType = "govscot:CovidRestrictionsLookup")
+public class CovidRestrictionsLookup extends SimpleContent {
+}
