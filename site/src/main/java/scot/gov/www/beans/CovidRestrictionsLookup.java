88042f88423fcd7b0154ce16896483390be96f04
Michael Irwin <michael.irwin@gov.scot>
2020-10-27 17:12:39 +0000

    MGS-6096 Added secondary content field
    Adds a secondary content field to the doctype

--- site/src/main/java/scot/gov/www/beans/CovidRestrictionsLookup.java
+++ site/src/main/java/scot/gov/www/beans/CovidRestrictionsLookup.java
@@ -1,9 +1,21 @@
 package scot.gov.www.beans;
 
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
 import org.hippoecm.hst.content.beans.Node;
+import java.util.List;
+import scot.gov.www.beans.UpdateHistory;
 
 @HippoEssentialsGenerated(internalName = "govscot:CovidRestrictionsLookup")
 @Node(jcrType = "govscot:CovidRestrictionsLookup")
 public class CovidRestrictionsLookup extends SimpleContent {
+    @HippoEssentialsGenerated(internalName = "govscot:secondaryContent")
+    public HippoHtml getSecondaryContent() {
+        return getHippoHtml("govscot:secondaryContent");
+    }
+
+    @HippoEssentialsGenerated(internalName = "govscot:updateHistory")
+    public List<UpdateHistory> getUpdateHistory() {
+        return getChildBeansByName("govscot:updateHistory", UpdateHistory.class);
+    }
 }
