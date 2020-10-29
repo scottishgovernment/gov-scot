dc8f2319661e2df472fd4d03536f0adae034d96b
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2020-10-29 13:27:25 +0000

    Add polyfill for element.closest()

--- repository-data/webfiles/src/scripts/govscot/format.covid-lookup.js
+++ repository-data/webfiles/src/scripts/govscot/format.covid-lookup.js
@@ -2,6 +2,28 @@
 
 import 'promise-polyfill/src/polyfill';
 
+// polyfill for element.closest
+if (!Element.prototype.matches) {
+    Element.prototype.matches =
+        Element.prototype.msMatchesSelector ||
+        Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+    Element.prototype.closest = function(s) {
+        var el = this;
+
+        do {
+            if (Element.prototype.matches.call(el, s)) {
+                return el;
+            }
+            el = el.parentElement || el.parentNode;
+        } while (el !== null && el.nodeType === 1);
+        return null;
+    };
+}
+
+
 const locationTitleTemplate = function (restriction) {
     let titleParts = [];
     titleParts.push(`<span data-${restriction.type}="${restriction.title}">${restriction.title}</span>`);
