bc38cf0238e30e06d443f9e9f491e6c4129603d7
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2021-12-07 15:31:05 +0000

    Add DS tracking

--- repository-data/webfiles/src/scripts/govscot/global.js
+++ repository-data/webfiles/src/scripts/govscot/global.js
@@ -48,6 +48,31 @@ const global = {
         this.initPubsub();
         this.svgFallback();
         this.initDesignSystemComponents();
+        this.addTracking();
+    },
+
+    addTracking: function () {
+        if (window.DS.tracking) {
+            window.DS.tracking.add.backtotop = function (scope = document) {
+                const backToTops = [].slice.call(scope.querySelectorAll('.ds_back-to-top__button'));
+                backToTops.forEach(backToTop => {
+                    if (!backToTop.classList.contains('js-has-tracking-event')) {
+                        backToTop.addEventListener('click', () => {
+                            window.dataLayer = window.dataLayer || [];
+                            window.dataLayer.push({
+                                event: 'backToTop',
+                                scrollDepthAbs: window.scrollY,
+                                scrollDepthRel: +(window.scrollY / window.innerHeight).toFixed(3)
+                            });
+                        });
+                        backToTop.classList.add('js-has-tracking-event');
+                    }
+                });
+                window.DS.tracking.add.backToTop(scope);
+            };
+
+            window.DS.tracking.init();
+        }
     },
 
     initDesignSystemComponents: function () {
