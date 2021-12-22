dc5fb79b6358472a94c15ee44f87424fc796bc4b
David Sinclair <david.sinclair@gov.scot>
2021-12-22 14:33:40 +0000

    MGS-6645 add event listener to maintain displayDate

--- cms/src/main/java/scot/gov/www/PublicationDisplayDateDaemonModule.java
+++ cms/src/main/java/scot/gov/www/PublicationDisplayDateDaemonModule.java
@@ -2,6 +2,8 @@ package scot.gov.www;
 
 import org.apache.commons.lang.StringUtils;
 import org.onehippo.repository.events.HippoWorkflowEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import scot.gov.publications.hippo.HippoUtils;
 
 import javax.jcr.Node;
@@ -14,6 +16,8 @@ import java.util.Calendar;
  * updateHistory.
  */
 public class PublicationDisplayDateDaemonModule extends DaemonModuleBase {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PublicationDisplayDateDaemonModule.class);
 
     private static final String PREFIX = "/content/documents/govscot/publications/";
 
@@ -38,7 +42,7 @@ public class PublicationDisplayDateDaemonModule extends DaemonModuleBase {
 
         // some event have the
         if (subject.isNodeType("hippo:handle")) {
-            publication = getLatestVariant(subject);
+            publication = new HippoUtils().getVariant(subject.getNodes());
         }
 
         if (publication == null) {
@@ -49,15 +53,21 @@ public class PublicationDisplayDateDaemonModule extends DaemonModuleBase {
     }
 
     void maintainDisplayDate(Node publication) throws RepositoryException {
-
-        Calendar currentDisplayDate = publication.getProperty(DISPLAY_DATE).getDate();
         Calendar newDisplayDate = getDisplayDate(publication);
 
-        // only do a save if the latest dat ahas actually changed
-        if (!newDisplayDate.equals(currentDisplayDate)) {
+        // only do a save if the latest date has actually changed
+        if (shouldUpdate(publication, newDisplayDate)) {
             setDisplayDate(publication, newDisplayDate);
             return;
         }
+    }
+
+    boolean shouldUpdate(Node publication, Calendar newDisplayDate) throws RepositoryException {
+        if (!publication.hasProperty(DISPLAY_DATE)) {
+            return true;
+        }
+        Calendar currentDisplayDate = publication.getProperty(DISPLAY_DATE).getDate();
+        return !newDisplayDate.equals(currentDisplayDate);
     }
 
     void setDisplayDate(Node publication, Calendar date) throws RepositoryException {
@@ -65,11 +75,15 @@ public class PublicationDisplayDateDaemonModule extends DaemonModuleBase {
                 publication.getParent().getNodes(publication.getName()),
                 pub -> pub.setProperty(DISPLAY_DATE, date)
         );
+
         session.save();
     }
 
-    Calendar getDisplayDate(Node publication) throws RepositoryException {
-        return publication.hasProperty(UPDATE_HISTORY)
+    Calendar getDisplayDate( Node publication) throws RepositoryException {
+
+        LOG.info("getDisplayDate has hsit? {} {}", publication.getPath(), publication.hasProperty(UPDATE_HISTORY));
+
+        return publication.hasNode(UPDATE_HISTORY)
                 ? getMostRecentDateFromUpdateHistory(publication)
                 : first(publication,
                         "govscot:publicationDate", "hippostdpubwf:publicationDate", "hippostdpubwf:lastModificationDate");
@@ -77,14 +91,20 @@ public class PublicationDisplayDateDaemonModule extends DaemonModuleBase {
 
     Calendar first(Node node, String ...props) throws RepositoryException {
         for (String prop : props) {
-            if (node.hasProperty(prop)) {
+            if (useProperty(node, prop)) {
                 return node.getProperty(prop).getDate();
             }
         }
         return null;
     }
 
+    boolean useProperty(Node node, String prop) throws RepositoryException {
+        // 'empty' dates are set to year 1, we want to treat these as null / empty
+        return node.hasProperty(prop) && node.getProperty(prop).getDate().get(Calendar.YEAR) != 1;
+    }
+
     Calendar getMostRecentDateFromUpdateHistory(Node publication) throws RepositoryException {
+        LOG.info("getMostRecentDateFromUpdateHistory");
         NodeIterator it = publication.getNodes(UPDATE_HISTORY);
         Node mostRecent = it.nextNode();
         while (it.hasNext()) {
@@ -97,24 +117,10 @@ public class PublicationDisplayDateDaemonModule extends DaemonModuleBase {
     }
 
     boolean isAfter(Node currentUpdate, Node newUpdate) throws RepositoryException {
-        return lastUpdated(newUpdate)
-                .after(lastUpdated(currentUpdate));
+        return lastUpdated(newUpdate).after(lastUpdated(currentUpdate));
     }
 
     Calendar lastUpdated(Node node) throws RepositoryException {
         return node.getProperty("govscot:lastUpdated").getDate();
     }
-
-    private static Node getLatestVariant(Node handle) throws RepositoryException {
-        NodeIterator it = handle.getNodes();
-        Node variant = null;
-        while (it.hasNext()) {
-            Node next = it.nextNode();
-            if (!next.isNodeType("hippostdpubwf:request")) {
-                variant = next;
-            }
-        }
-        return variant;
-    }
-
 }
