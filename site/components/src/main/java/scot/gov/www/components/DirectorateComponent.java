e76365e5de4b61088384f3957239c257d9494210
David Sinclair <david.sinclair@gov.scot>
2022-01-17 15:10:27 +0000

    MGS-6659 non existent directorates now 404 correctly

--- site/components/src/main/java/scot/gov/www/components/DirectorateComponent.java
+++ site/components/src/main/java/scot/gov/www/components/DirectorateComponent.java
@@ -5,6 +5,7 @@ import org.hippoecm.hst.content.beans.query.HstQuery;
 import org.hippoecm.hst.content.beans.query.HstQueryResult;
 import org.hippoecm.hst.content.beans.query.exceptions.QueryException;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.core.component.HstComponentException;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
 import org.hippoecm.hst.core.request.HstRequestContext;
@@ -14,16 +15,21 @@ import org.slf4j.LoggerFactory;
 import scot.gov.www.beans.Directorate;
 import scot.gov.www.beans.Policy;
 
+import java.io.IOException;
+
 public class DirectorateComponent extends BaseHstComponent {
 
     private static final Logger LOG = LoggerFactory.getLogger(DirectorateComponent.class);
 
     @Override
-    public void doBeforeRender(final HstRequest request,
-                               final HstResponse response) {
-
+    public void doBeforeRender(HstRequest request, HstResponse response) {
+        super.doBeforeRender(request, response);
         HstRequestContext context = request.getRequestContext();
         Directorate document = context.getContentBean(Directorate.class);
+        if (document == null) {
+            forwardTo404Page(request, response);
+            return;
+        }
         request.setAttribute("document", document);
 
         // find all policies that link to this Directorate
@@ -36,6 +42,16 @@ public class DirectorateComponent extends BaseHstComponent {
             request.setAttribute("policies", policies.getHippoBeans());
         } catch (QueryException e) {
             LOG.warn("Unable to get Policies for directorate {}", document.getPath(), e);
+        }
+    }
+
+    void forwardTo404Page(HstRequest request, HstResponse response) {
+        try {
+            LOG.info("404 for {}", request.getRequestURL());
+            response.setStatus(404);
+            response.forward("/pagenotfound");
+        }  catch (IOException e) {
+            throw new HstComponentException("forward failed", e);
         }
     }
 
