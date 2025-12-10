98dcba8b8b615d42a09c33792b669076f58651ab
Katie Baigrie <katie.baigrie@gov.scot>
2025-12-10 08:48:42 +0000

    MGS-8199 Fix to disambiguate Vuelio impotered content with same title in the same month

--- vuelio-importer/src/main/java/scot/gov/www/importer/sink/Locations.java
+++ vuelio-importer/src/main/java/scot/gov/www/importer/sink/Locations.java
@@ -1,10 +1,8 @@
 package scot.gov.www.importer.sink;
 
-import com.fasterxml.jackson.databind.type.LogicalType;
 import com.github.slugify.Slugify;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import scot.gov.www.importer.VuelioImporter;
 import scot.gov.www.importer.domain.PressRelease;
 
 import javax.jcr.Node;
@@ -30,7 +28,7 @@ public class Locations {
     }
 
     public String newsLocation(PressRelease release, Session session) throws RepositoryException {
-        // if a press release exists with this external id then ise this as the path - this will ensure the item is reused instead
+        // if a press release exists with this external id then use this as the path - this will ensure the item is reused instead
         // of a new one being created in the case where the title has changes.
         Node existing = getByExternalId(session, GOVSCOT_NEWS, release);
         if (existing != null) {
@@ -41,16 +39,18 @@ public class Locations {
         String yearString = Integer.toString(release.getDateTime().getYear());
         String monthString = String.format("%02d", release.getDateTime().getMonthValue());
         String location = String.format("/content/documents/govscot/news/%s/%s/%s", yearString, monthString, slug);
-        if (session.nodeExists(location)) {
-            // if a press release with the same title had been created in the same month then we need to disambiguate
-            location = String.format("/content/documents/govscot/news/%s/%s/%s-%s", yearString, monthString, slug, release.getId());
-            LOG.info("duplicate news title found, disambiguating to: {}", location);
+
+        if (!locationAlreadyExists(session, location)) {
+            // if a press release has a unique title for the month then return
+            LOG.info("----> News location {}", location);
+            return location;
         }
-        return location;
+
+        return disambiguate(session, location, 2);
     }
 
     public String publicationLocation(PressRelease release, String publicationType, Session session) throws RepositoryException {
-        // if a press release exists with this external id then ise this as the path - this will ensure the item is reusled instead
+        // if a press release exists with this external id then ise this as the path - this will ensure the item is reused instead
         // of a new one being created in the case where the title has changes.
         Node existing = getByExternalId(session, GOVSCOT_PUBLICATION, release);
         if (existing != null) {
@@ -60,15 +60,15 @@ public class Locations {
         String slug = slugify.slugify(release.getTitle());
         String yearString = Integer.toString(release.getDateTime().getYear());
         String monthString = String.format("%02d", release.getDateTime().getMonthValue());
+        String location = String.format("/content/documents/govscot/publications/%s/%s/%s/%s", publicationType, yearString, monthString, slug);
 
-        String location = String.format("/content/documents/govscot/publications/%s/%s/%s/%s/index", publicationType, yearString, monthString, slug);
-        if (session.nodeExists(location)) {
-            // if a press release with the same title had been created in the same month then we need to disambiguate
-            location = String.format("/content/documents/govscot/publications/%s/%s/%s/%s%s/index", publicationType, yearString, monthString, slug, release.getId());
-            LOG.info("------> duplicate publication title found, disambiguating to: {}", location);
+        if (!locationAlreadyExists(session, location)) {
+            LOG.info("----> Publication location {}", location);
+            return location + "/index";
         }
-LOG.info("----> location {}", location);
-        return location;
+
+        return disambiguate(session, location, 2) + "/index";
+
     }
 
     public static Node getByExternalId(Session session, String type, PressRelease pressRelease) throws RepositoryException {
@@ -80,5 +80,22 @@ LOG.info("----> location {}", location);
         } else {
             return null;
         }
+    }
+
+    private String disambiguate(Session session, String location, int postfix) throws RepositoryException {
+        String candidate = String.format("%s-%d", location, postfix);
+
+        if (!locationAlreadyExists(session, candidate)) {
+            // Base case: we have found a unique slug.
+            LOG.info("----> Disambiguated location {}", candidate);
+            return candidate;
+        }
+
+        // Recursive call to try the next number.
+        return disambiguate(session, location,postfix + 1);
+    }
+
+    private boolean locationAlreadyExists(Session session, String location) throws RepositoryException {
+        return session.nodeExists(location);
     }
 }
