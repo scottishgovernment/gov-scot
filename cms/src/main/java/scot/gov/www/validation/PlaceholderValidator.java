460ed09eadb1ded6b2b8aae1bf1fccd0f4145b8b
David Sinclair <david.sinclair@gov.scot>
2026-01-28 14:00:46 +0000

    MGS-8182 updates to Minutes format - add requested format specifc tooltips and placeholder content - remove fields not used in minutes - add documentation link - add validator for placeholder content

---
+++ cms/src/main/java/scot/gov/www/validation/PlaceholderValidator.java
@@ -0,0 +1,23 @@
+package scot.gov.www.validation;
+
+import org.onehippo.cms.services.validation.api.ValidationContext;
+import org.onehippo.cms.services.validation.api.Validator;
+import org.onehippo.cms.services.validation.api.Violation;
+
+import java.util.Optional;
+
+public class PlaceholderValidator implements Validator<String> {
+
+    @Override
+    public Optional<Violation> validate(ValidationContext context, String value) {
+        if (value == null || value.isEmpty()) {
+            return Optional.empty();
+        }
+
+        if (value.matches(".*\\[.*?\\].*")) {
+            return Optional.of(context.createViolation());
+        }
+
+        return Optional.empty();
+    }
+}
